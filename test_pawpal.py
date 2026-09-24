35d8c34df443b95bc40ced6aa37e477201ce4f00
DoodleTDT <tiot1833@gmail.com>
2026-09-24 15:17:23 -0500

    Added to test file

---
+++ test_pawpal.py
@@ -0,0 +1,11 @@
+"""
+This is where to test the code from pawpal_system.py and main.py
+"""
+from datetime import time
+from pawpal_system import User, Pet, Task, Schedule
+
+
+# Pytest: Verify that calling mark_complete() actually changes the task's status.
+
+
+# Pytest: Verify that adding a task to a Pet increases that pet's task count.
